b9c68ace2fbdb9b5f866e40c58d498940c2c912e
zhuziba <96967229+zhuziba@users.noreply.github.com>
2022-05-16 13:16:28 +0800

    Update entrypoint.sh

--- v2raya/entrypoint.sh
+++ v2raya/entrypoint.sh
@@ -13,5 +13,12 @@ if [ ! -e '/usr/share/v2ray/v2ray' ]; then
     rm -rf /tmp/v2ray
     echo "下载v2ray完成"
 fi
+
+if [ ! -e '/usr/bin/v2raya' ]; then
+    if [ $(arch) == aarch64 ]; then      wget https://download.fastgit.org/v2rayA/v2rayA/releases/download/v$VER/v2raya_linux_arm64_$VER;     chmod +x /v2raya_linux_arm64_$VER;     mv /v2raya_linux_arm64_$VER /usr/bin/v2raya; fi
+    if [ $(arch) == x86_64 ]; then     wget https://download.fastgit.org/v2rayA/v2rayA/releases/download/v$VER/v2raya_linux_x64_$VER;     chmod +x /v2raya_linux_x64_$VER;     mv /v2raya_linux_x64_$VER /usr/bin/v2raya; fi
+    if [ $(arch) == armv7l ]; then     wget https://download.fastgit.org/v2rayA/v2rayA/releases/download/v$VER/v2raya_linux_arm_$VER;     chmod +x /v2raya_linux_arm_$VER;     mv /v2raya_linux_arm_$VER /usr/bin/v2raya; fi
+    echo "下载v2rayA完成"
+fi
 v2raya
 #v2raya --log-level error --log-file /var/log/v2raya.log
