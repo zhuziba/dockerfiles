22ed181d7e6072e275ea88b6c5c214c987c6dc40
zhuziba <96967229+zhuziba@users.noreply.github.com>
2022-06-06 02:26:47 +0800

    Update entrypoint.sh

--- v2raya/entrypoint.sh
+++ v2raya/entrypoint.sh
@@ -8,8 +8,8 @@ if [ ! -e '/usr/share/v2ray/v2ray' ]; then
     mv /tmp/v2ray/v2ray /usr/share/v2ray/v2ray
     mv /tmp/v2ray/geoip.dat /usr/local/share/v2ray/geoip.dat
     mv /tmp/v2ray/geosite.dat /usr/local/share/v2ray/geosite.dat
-    wget -O /tmp/v2ray/LoyalsoldierSite.dat https://raw.fastgit.org/mzz2017/dist-v2ray-rules-dat/master/geosite.dat
-    mv /tmp/v2ray/LoyalsoldierSite.dat /usr/local/share/v2ray/LoyalsoldierSite.dat
+    wget -O /usr/local/share/v2ray/LoyalsoldierSite.dat https://download.fastgit.org/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
+    wget -O /usr/local/share/v2ray/Loyalsoldierip.datt https://download.fastgit.org/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
     rm -rf /tmp/v2ray
     echo "下载v2ray完成"
 fi
@@ -20,5 +20,4 @@ if [ ! -e '/usr/bin/v2raya' ]; then
     if [ $(arch) == armv7l ]; then     wget https://download.fastgit.org/v2rayA/v2rayA/releases/download/v$VER/v2raya_linux_arm_$VER;     chmod +x /v2raya_linux_arm_$VER;     mv /v2raya_linux_arm_$VER /usr/bin/v2raya; fi
     echo "下载v2rayA完成"
 fi
-v2raya
-#v2raya --log-level error --log-file /var/log/v2raya.log
+v2raya --log-level error --log-file /var/log/v2raya.log
