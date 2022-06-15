6eed5eb32cbc6477210372ee066d3f10a2433576
zhuziba <96967229+zhuziba@users.noreply.github.com>
2022-06-15 15:00:14 +0800

    Update entrypoint.sh

--- v2raya/entrypoint.sh
+++ v2raya/entrypoint.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+if [ ! -e '/usr/share/v2ray/v2ray' ]; then
+    if [ $(arch) == aarch64 ]; then      wget -O /tmp/v2ray/v2ray.zip https://download.fastgit.org/v2fly/v2ray-core/releases/latest/download/v2ray-linux-arm64-v8a.zip; fi
+    if [ $(arch) == x86_64 ]; then     wget -O /tmp/v2ray/v2ray.zip https://download.fastgit.org/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip; fi
+    if [ $(arch) == armv7l ]; then     wget -O /tmp/v2ray/v2ray.zip https://download.fastgit.org/v2fly/v2ray-core/releases/latest/download/v2ray-linux-arm32-v7a.zip; fi
+    unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
+    chmod +x /tmp/v2ray/v2ray
+    mv /tmp/v2ray/v2ray /usr/share/v2ray/v2ray
+    mv /tmp/v2ray/geoip.dat /usr/local/share/v2ray/geoip.dat
+    mv /tmp/v2ray/geosite.dat /usr/local/share/v2ray/geosite.dat
+    wget -O /usr/local/share/v2ray/LoyalsoldierSite.dat https://download.fastgit.org/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
+    wget -O /usr/local/share/v2ray/Loyalsoldierip.dat https://download.fastgit.org/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
+    rm -rf /tmp/v2ray
+    echo "下载v2ray完成"
+fi
+
 if [ ! -e '/usr/bin/v2raya' ]; then
     wget -O /usr/local/share/v2ray/LoyalsoldierSite.dat https://download.fastgit.org/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
     wget -O /usr/local/share/v2ray/Loyalsoldierip.dat https://download.fastgit.org/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
