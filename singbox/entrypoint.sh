2149167a28c20ec771dc0829bc011488b8c9cbcb
zhuziba <96967229+zhuziba@users.noreply.github.com>
2022-11-08 02:48:36 +0800

    Update entrypoint.sh

--- singbox/entrypoint.sh
+++ singbox/entrypoint.sh
@@ -8,8 +8,8 @@ echo -e "======================== 0.1 判断是否安装sing-box文件 =========
 if [ ! -e '/usr/bin/sing-box' ]; then
     singbox=1.1-beta12
     echo "当前获取sing-box版本为$singbox"
-    if [ $(arch) == x86_64 ]; then     wget -P /tmp  https://download.nuaa.cf/SagerNet/sing-box/releases/download/$sing-box/sing-box-$singbox-linux-amd64.tar.gz;     tar -xvf /tmp/sing-box-$sing-box-linux-amd64.tar.gz;     mv /tmp/sing-box-$singbox-linux-amd64/sing-box /usr/bin/sing-box;     chmod +x /usr/bin/sing-box; fi
-    if [ $(arch) == aarch64 ]; then    wget -P /tmp  https://download.nuaa.cf/SagerNet/sing-box/releases/download/$sing-box/sing-box-$singbox-linux-arm64.tar.gz;     tar -xvf /tmp/sing-box-$sing-box-linux-arm64.tar.gz;     mv /tmp/sing-box-$singbox-linux-arm64/sing-box /usr/bin/sing-box;     chmod +x /usr/bin/sing-box; fi
+    if [ $(arch) == x86_64 ]; then     wget -P /tmp  https://download.nuaa.cf/SagerNet/sing-box/releases/download/$singbox/sing-box-$singbox-linux-amd64.tar.gz;     tar -xvf /tmp/sing-box-$sing-box-linux-amd64.tar.gz;     mv /tmp/sing-box-$singbox-linux-amd64/sing-box /usr/bin/sing-box;     chmod +x /usr/bin/sing-box; fi
+    if [ $(arch) == aarch64 ]; then    wget -P /tmp  https://download.nuaa.cf/SagerNet/sing-box/releases/download/$singbox/sing-box-$singbox-linux-arm64.tar.gz;     tar -xvf /tmp/sing-box-$sing-box-linux-arm64.tar.gz;     mv /tmp/sing-box-$singbox-linux-arm64/sing-box /usr/bin/sing-box;     chmod +x /usr/bin/sing-box; fi
     echo "下载sing-box完成"
 
 fi
