6d7142adbdc05de2446cee5c03be365fa9cbcca0
zhuziba <96967229+zhuziba@users.noreply.github.com>
2022-11-09 20:38:16 +0800

    Update entrypoint.sh

--- singbox/entrypoint.sh
+++ singbox/entrypoint.sh
@@ -3,5 +3,12 @@
 sed -i "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g" /etc/sysctl.conf
 sysctl -p
 
+case $down_type in
+"git")
+   wget -P /singbox ${down_url}
+  break
+  ;;
+esac
+
 sing-box run -c /singbox/config.json
 tail -f /dev/null
