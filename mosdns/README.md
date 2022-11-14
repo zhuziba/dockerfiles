52b0707eb157f8374a49af7b3b9383f5ab408b91
zhuziba <96967229+zhuziba@users.noreply.github.com>
2022-11-15 00:36:41 +0800

    Add files via upload

---
+++ mosdns/README.md
@@ -0,0 +1,26 @@
+# mosdns
+
+clash
+开启混杂模式
+``` sh
+ip link set eth0 promisc on
+```
+docker创建网络,注意将网段改为你自己的
+``` sh
+docker network create -d macvlan --subnet=192.168.50.0/24 --gateway=192.168.50.1 -o parent=eth0 macnet
+```
+# 推荐部署容器命令
+``` sh
+docker run --restart=always \
+  -d --name mosdns \
+  --privileged=true \
+  --net macnet \
+  --ip 192.168.50.5 \
+  -v /docker/mosdns:/mosdns \
+  byxiaopeng/mosdns:latest
+```
+
+重启clash容器代码
+``` sh
+docker restart mosdns
+```
---
+++ mosdns/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# 开启转发
+sed -i "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g" /etc/sysctl.conf
+sysctl -p
+
+echo -e "======================== 0.1 判断是否安装mosdns文件 ========================\n"
+
+if [ ! -e '/usr/bin/mosdns' ]; then
+    if [ $(arch) == x86_64 ]; then    wget -P /tmp  https://github.com/IrineSistiana/mosdns/releases/latest/download/mosdns-linux-amd64.zip;     unzip /tmp/mosdns-linux-amd64.zip;     mv /tmp/mosdns /usr/bin/mosdns;     chmod +x /usr/bin/mosdns; fi
+    if [ $(arch) == aarch64 ]; then    wget -P /tmp  https://github.com/IrineSistiana/mosdns/releases/latest/download/mosdns-linux-arm64.zip;     unzip /tmp/mosdns-linux-arm64.zip;     mv /tmp/mosdns /usr/bin/mosdns;     chmod +x /usr/bin/mosdns; fi
+    echo "下载mosdns完成"
+fi
+
+mosdns start --dir /mosdns
+tail -f /dev/null
