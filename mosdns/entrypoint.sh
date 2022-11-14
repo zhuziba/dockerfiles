754d783c2431eeec228ac171b840e1c664282ded
zhuziba <96967229+zhuziba@users.noreply.github.com>
2022-11-15 01:27:57 +0800

    Update entrypoint.sh

--- mosdns/entrypoint.sh
+++ mosdns/entrypoint.sh
@@ -6,8 +6,8 @@ sysctl -p
 echo -e "======================== 0.1 判断是否安装mosdns文件 ========================\n"
 
 if [ ! -e '/usr/bin/mosdns' ]; then
-    if [ $(arch) == x86_64 ]; then    wget -P /tmp  https://github.com/IrineSistiana/mosdns/releases/latest/download/mosdns-linux-amd64.zip;     unzip /tmp/mosdns-linux-amd64.zip;     mv /tmp/mosdns /usr/bin/mosdns;     chmod +x /usr/bin/mosdns; fi
-    if [ $(arch) == aarch64 ]; then    wget -P /tmp  https://github.com/IrineSistiana/mosdns/releases/latest/download/mosdns-linux-arm64.zip;     unzip /tmp/mosdns-linux-arm64.zip;     mv /tmp/mosdns /usr/bin/mosdns;     chmod +x /usr/bin/mosdns; fi
+    if [ $(arch) == x86_64 ]; then    wget -P /tmp  https://github.com/IrineSistiana/mosdns/releases/latest/download/mosdns-linux-amd64.zip;     unzip /tmp/mosdns-linux-amd64.zip;     mv /tmp/mosdns /usr/bin/mosdns -d /tmp;     chmod +x /usr/bin/mosdns; fi
+    if [ $(arch) == aarch64 ]; then    wget -P /tmp  https://github.com/IrineSistiana/mosdns/releases/latest/download/mosdns-linux-arm64.zip;     unzip /tmp/mosdns-linux-arm64.zip;     mv /tmp/mosdns /usr/bin/mosdns -d /tmp;     chmod +x /usr/bin/mosdns; fi
     echo "下载mosdns完成"
 fi
 
