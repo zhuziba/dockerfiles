f9c952fd6847c6eeccbb372afa4e168cdb75fd69
zhuziba <96967229+zhuziba@users.noreply.github.com>
2022-11-19 03:12:07 +0800

    Update entrypoint.sh

--- sillygirl/entrypoint.sh
+++ sillygirl/entrypoint.sh
@@ -4,7 +4,8 @@
 echo -e "======================== 0.1 判断是否安装傻妞文件 ========================\n"
 
 if [ ! -e '/usr/bin/sillyGirl' ]; then
-    time=curl https://cdn.staticaly.com/gh/cdle/binary/main/compile_time.go --silent | tr -cd "[0-9]"
+    time=`curl https://cdn.staticaly.com/gh/cdle/binary/main/compile_time.go --silent | tr -cd "[0-9]"`
+	echo "当前获取sillyGirl版本为$time"
     if [ $(arch) == x86_64 ]; then    wget -P /tmp  https://cdn.staticaly.com/gh/cdle/binary/main/sillyGirl_linux_amd64_${time};     mv /tmp/sillyGirl_linux_amd64_${time} /usr/bin/sillyGirl;     chmod +x /usr/bin/sillyGirl; fi
     if [ $(arch) == aarch64 ]; then    wget -P /tmp  https://cdn.staticaly.com/gh/cdle/binary/main/sillyGirl_linux_arm64_${time};     mv /tmp/sillyGirl_linux_arm64_${time} /usr/bin/sillyGirl;     chmod +x /usr/bin/sillyGirl; fi
     echo "下载sillyGirl完成"
