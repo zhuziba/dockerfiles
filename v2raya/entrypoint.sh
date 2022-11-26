710c80421c31b028ad3ae0834f93cdac681375cf
zhuziba <96967229+zhuziba@users.noreply.github.com>
2022-11-26 17:21:38 +0800

    Update entrypoint.sh

--- v2raya/entrypoint.sh
+++ v2raya/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ ! -e '/usr/share/xray/xray' ]; then
-    if [ $(arch) == aarch64 ]; then      wget -O /tmp/xray/Xray.zip https://download.fastgit.org/XTLS/Xray-core/releases/latest/download/Xray-linux-arm64-v8a.zip; fi
-    if [ $(arch) == x86_64 ]; then     wget -O /tmp/xray/Xray.zip https://download.fastgit.org/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip; fi
-    if [ $(arch) == armv7l ]; then     wget -O /tmp/xray/Xray.zip https://download.fastgit.org/XTLS/Xray-core/releases/latest/download/Xray-linux-arm32-v7a.zip; fi
+    if [ $(arch) == aarch64 ]; then      wget -O /tmp/xray/Xray.zip https://kgithub.com/XTLS/Xray-core/releases/latest/download/Xray-linux-arm64-v8a.zip; fi
+    if [ $(arch) == x86_64 ]; then     wget -O /tmp/xray/Xray.zip https://kgithub.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip; fi
+    if [ $(arch) == armv7l ]; then     wget -O /tmp/xray/Xray.zip https://kgithub.com/XTLS/Xray-core/releases/latest/download/Xray-linux-arm32-v7a.zip; fi
     unzip /tmp/xray/Xray.zip -d /tmp/xray
     chmod +x /tmp/xray/xray
     mv /tmp/xray/xray /usr/share/xray/xray
@@ -34,9 +34,9 @@ if [ ! -e '/usr/local/share/xray/Loyalsoldierip.dat' ]; then
 fi
 
 if [ ! -e '/usr/bin/v2raya' ]; then
-    if [ $(arch) == aarch64 ]; then      wget https://download.fastgit.org/v2rayA/v2rayA/releases/download/v$VER/v2raya_linux_arm64_$VER;     chmod +x /v2raya_linux_arm64_$VER;     mv /v2raya_linux_arm64_$VER /usr/bin/v2raya; fi
-    if [ $(arch) == x86_64 ]; then     wget https://download.fastgit.org/v2rayA/v2rayA/releases/download/v$VER/v2raya_linux_x64_$VER;     chmod +x /v2raya_linux_x64_$VER;     mv /v2raya_linux_x64_$VER /usr/bin/v2raya; fi
-    if [ $(arch) == armv7l ]; then     wget https://download.fastgit.org/v2rayA/v2rayA/releases/download/v$VER/v2raya_linux_arm_$VER;     chmod +x /v2raya_linux_arm_$VER;     mv /v2raya_linux_arm_$VER /usr/bin/v2raya; fi
+    if [ $(arch) == aarch64 ]; then      wget https://kgithub.com/v2rayA/v2rayA/releases/download/v$VER/v2raya_linux_arm64_$VER;     chmod +x /v2raya_linux_arm64_$VER;     mv /v2raya_linux_arm64_$VER /usr/bin/v2raya; fi
+    if [ $(arch) == x86_64 ]; then     wget https://kgithub.com/v2rayA/v2rayA/releases/download/v$VER/v2raya_linux_x64_$VER;     chmod +x /v2raya_linux_x64_$VER;     mv /v2raya_linux_x64_$VER /usr/bin/v2raya; fi
+    if [ $(arch) == armv7l ]; then     wget https://kgithub.com/v2rayA/v2rayA/releases/download/v$VER/v2raya_linux_arm_$VER;     chmod +x /v2raya_linux_arm_$VER;     mv /v2raya_linux_arm_$VER /usr/bin/v2raya; fi
     echo "下载v2rayA完成"
 fi
 
@@ -44,9 +44,9 @@ if [ ! -e '/etc/v2raya/smartdns.conf' ]; then
     echo "smartdns.conf文件不存在不启动smartdns"
     else
     echo "smartdns.conf文件存在启动smartdns"
-    if [ $(arch) == aarch64 ]; then      wget -O /tmp/smartdns https://download.fastgit.org/pymumu/smartdns/releases/latest/download/smartdns-aarch64; fi
-    if [ $(arch) == x86_64 ]; then     wget -O /tmp/smartdns https://download.fastgit.org/pymumu/smartdns/releases/latest/download/smartdns-x86_64; fi
-    if [ $(arch) == armv7l ]; then     wget -O /tmp/smartdns https://download.fastgit.org/pymumu/smartdns/releases/latest/download/smartdns-arm; fi
+    if [ $(arch) == aarch64 ]; then      wget -O /tmp/smartdns https://kgithub.com/pymumu/smartdns/releases/latest/download/smartdns-aarch64; fi
+    if [ $(arch) == x86_64 ]; then     wget -O /tmp/smartdns https://kgithub.com/pymumu/smartdns/releases/latest/download/smartdns-x86_64; fi
+    if [ $(arch) == armv7l ]; then     wget -O /tmp/smartdns https://kgithub.com/pymumu/smartdns/releases/latest/download/smartdns-arm; fi
     chmod +x /tmp/smartdns
     mv /tmp/smartdns /usr/bin/smartdns
     echo "下载smartdns完成"
