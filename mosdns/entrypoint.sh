c93c84c7445d55338a1b10e1c7638a3142dad9dd
zhuziba <96967229+zhuziba@users.noreply.github.com>
2022-12-08 04:29:23 +0800

    Update entrypoint.sh

--- mosdns/entrypoint.sh
+++ mosdns/entrypoint.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+echo "开始生成nginx配置文件"
+cat <<EOF> /etc/nginx/http.d/default.conf
+server {
+	listen 80 default_server;
+	#listen [::]:80 default_server;
+	charset utf-8;
+
+     location /dns-query {
+     set_real_ip_from 0.0.0.0/0;
+     real_ip_header X-Forwarded-For;
+     proxy_pass http://127.0.0.1:8888/dns-query;
+     }
+}
+EOF
+echo "启动nginx进程"
+nginx
 echo "每天2点自动下载geoip和geosite文件"
 echo '0 2 * * *  wget -O /mosdns/geoip.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat'>>/var/spool/cron/crontabs/root
 echo '0 2 * * *  wget -O /mosdns/geosite.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat'>>/var/spool/cron/crontabs/root
