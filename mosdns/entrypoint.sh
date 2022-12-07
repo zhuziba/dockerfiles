5f90b2467e7e268aeeb244aa1734b621827c191a
zhuziba <96967229+zhuziba@users.noreply.github.com>
2022-12-08 03:07:59 +0800

    Update entrypoint.sh

--- mosdns/entrypoint.sh
+++ mosdns/entrypoint.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-echo -e "======================== 0.1 判断是否安装mosdns文件 ========================\n"
-if [ ! -e '/usr/bin/mosdns' ]; then
-    if [ $(arch) == x86_64 ]; then    wget -P /tmp  https://kgithub.com/IrineSistiana/mosdns/releases/latest/download/mosdns-linux-amd64.zip;     unzip /tmp/mosdns-linux-amd64.zip -d /tmp;     rm -rf /tmp/mosdns-linux-arm64.zip;     mv /tmp/mosdns /usr/bin/mosdns;     chmod +x /usr/bin/mosdns; fi
-    if [ $(arch) == aarch64 ]; then    wget -P /tmp  https://kgithub.com/IrineSistiana/mosdns/releases/latest/download/mosdns-linux-arm64.zip;     unzip /tmp/mosdns-linux-arm64.zip -d /tmp;     rm -rf /tmp/mosdns-linux-arm64.zip;     mv /tmp/mosdns /usr/bin/mosdns;     chmod +x /usr/bin/mosdns; fi
-    echo "下载mosdns完成"
-fi
 echo "每天2点自动下载geoip和geosite文件"
 echo '0 2 * * *  wget -O /mosdns/geoip.dat https://kgithub.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat'>>/var/spool/cron/crontabs/root
 echo '0 2 * * *  wget -O /mosdns/geosite.dat https://kgithub.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat'>>/var/spool/cron/crontabs/root
