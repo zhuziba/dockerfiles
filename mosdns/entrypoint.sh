e8650016bb61c415faaead746ea2762125663686
zhuziba <96967229+zhuziba@users.noreply.github.com>
2022-12-08 04:20:21 +0800

    Update entrypoint.sh

--- mosdns/entrypoint.sh
+++ mosdns/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "每天2点自动下载geoip和geosite文件"
-echo '0 2 * * *  wget -O /mosdns/geoip.dat https://kgithub.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat'>>/var/spool/cron/crontabs/root
-echo '0 2 * * *  wget -O /mosdns/geosite.dat https://kgithub.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat'>>/var/spool/cron/crontabs/root
+echo '0 2 * * *  wget -O /mosdns/geoip.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat'>>/var/spool/cron/crontabs/root
+echo '0 2 * * *  wget -O /mosdns/geosite.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat'>>/var/spool/cron/crontabs/root
 echo "启动定时任务"
 crond -b -l 8
 echo "启动openrc后台启动"
