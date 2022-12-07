e065d5e78c414954a95b7f125618aa9bf039086e
zhuziba <96967229+zhuziba@users.noreply.github.com>
2022-12-07 09:03:41 +0800

    Update iptables.sh

--- clash/script/iptables.sh
+++ clash/script/iptables.sh
@@ -10,7 +10,14 @@ iptables -t mangle -A clash -d 172.16.0.0/12 -j RETURN
 iptables -t mangle -A clash -d 192.168.0.0/16 -j RETURN
 iptables -t mangle -A clash -d 224.0.0.0/4 -j RETURN
 iptables -t mangle -A clash -d 240.0.0.0/4 -j RETURN
+# FORWARD ALL
 iptables -t mangle -A clash -p tcp -j TPROXY --on-port 7893 --tproxy-mark 1
 iptables -t mangle -A clash -p udp -j TPROXY --on-port 7893 --tproxy-mark 1
+# REDIRECT
 iptables -t mangle -A PREROUTING -j clash
+# hijack DNS to Clash
+iptables -t nat -N CLASH_DNS
+iptables -t nat -F CLASH_DNS 
+iptables -t nat -A CLASH_DNS -p udp -j REDIRECT --to-port 1053
+iptables -t nat -I PREROUTING -p udp --dport 53 -j CLASH_DNS
 echo "执行路由表完毕"
