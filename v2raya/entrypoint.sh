ae4fa5197f2c716e2c932948226520baa52392e7
zhuziba <96967229+zhuziba@users.noreply.github.com>
2022-12-10 12:59:43 +0800

    Update entrypoint.sh

--- v2raya/entrypoint.sh
+++ v2raya/entrypoint.sh
@@ -1,36 +1,41 @@
 #!/bin/bash
 
-if [ ! -e '/usr/share/xray/xray' ]; then
-    if [ $(arch) == aarch64 ]; then      wget -O /tmp/xray/Xray.zip https://kgithub.com/XTLS/Xray-core/releases/latest/download/Xray-linux-arm64-v8a.zip; fi
-    if [ $(arch) == x86_64 ]; then     wget -O /tmp/xray/Xray.zip https://kgithub.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip; fi
-    if [ $(arch) == armv7l ]; then     wget -O /tmp/xray/Xray.zip https://kgithub.com/XTLS/Xray-core/releases/latest/download/Xray-linux-arm32-v7a.zip; fi
-    unzip /tmp/xray/Xray.zip -d /tmp/xray
-    chmod +x /tmp/xray/xray
-    mv /tmp/xray/xray /usr/share/xray/xray
-    mv /tmp/xray/geoip.dat /usr/local/share/xray/geoip.dat
-    mv /tmp/xray/geosite.dat /usr/local/share/xray/geosite.dat
-    rm -rf /tmp/xray
-    echo "下载xray完成"
+if [ ! -e '/usr/share/v2ray/v2ray' ]; then
+    v2ray=v5.1.0
+    echo "当前获取v2ray版本为$v2ray"
+    if [ $(arch) == aarch64 ]; then      wget -O /tmp/v2ray/v2ray.zip https://kgithub.com/v2fly/v2ray-core/releases/download/$v2ray/v2ray-linux-arm64-v8a.zip; fi
+    if [ $(arch) == x86_64 ]; then     wget -O /tmp/v2ray/v2ray.zip https://kgithub.com/v2fly/v2ray-core/releases/download/$v2ray/v2ray-linux-64.zip; fi
+    if [ $(arch) == armv7l ]; then     wget -O /tmp/v2ray/v2ray.zip https://kgithub.com/v2fly/v2ray-core/releases/download/$v2ray/v2ray-linux-arm32-v7a.zip; fi
+    #if [ $(arch) == aarch64 ]; then      wget -O /tmp/v2ray/v2ray.zip https://download.fastgit.org/v2fly/v2ray-core/releases/latest/download/v2ray-linux-arm64-v8a.zip; fi
+    #if [ $(arch) == x86_64 ]; then     wget -O /tmp/v2ray/v2ray.zip https://download.fastgit.org/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip; fi
+    #if [ $(arch) == armv7l ]; then     wget -O /tmp/v2ray/v2ray.zip https://download.fastgit.org/v2fly/v2ray-core/releases/latest/download/v2ray-linux-arm32-v7a.zip; fi
+    unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
+    chmod +x /tmp/v2ray/v2ray
+    mv /tmp/v2ray/v2ray /usr/share/v2ray/v2ray
+    mv /tmp/v2ray/geoip.dat /usr/local/share/v2ray/geoip.dat
+    mv /tmp/v2ray/geosite.dat /usr/local/share/v2ray/geosite.dat
+    rm -rf /tmp/v2ray
+    echo "下载v2ray完成"
 fi
 
-if [ ! -e '/usr/local/share/xray/LoyalsoldierSite.dat' ]; then
+if [ ! -e '/usr/local/share/v2ray/LoyalsoldierSite.dat' ]; then
     echo "下载LoyalsoldierSite.dat文件"
-    wget -O /usr/local/share/xray/LoyalsoldierSite.dat https://raw.iqiq.io/Loyalsoldier/v2ray-rules-dat/release/geosite.dat
+    wget -O /usr/local/share/v2ray/LoyalsoldierSite.dat https://raw.iqiq.io/Loyalsoldier/v2ray-rules-dat/release/geosite.dat
     else
     echo "LoyalsoldierSite.dat文件存在删除下载最新版本"
-    rm -rf /usr/local/share/xray/LoyalsoldierSite.dat
+    rm -rf /usr/local/share/v2ray/LoyalsoldierSite.dat
     echo "下载LoyalsoldierSite.dat文件"
-    wget -O /usr/local/share/xray/LoyalsoldierSite.dat https://raw.iqiq.io/Loyalsoldier/v2ray-rules-dat/release/geosite.dat
+    wget -O /usr/local/share/v2ray/LoyalsoldierSite.dat https://raw.iqiq.io/Loyalsoldier/v2ray-rules-dat/release/geosite.dat
 fi
 
-if [ ! -e '/usr/local/share/xray/Loyalsoldierip.dat' ]; then
+if [ ! -e '/usr/local/share/v2ray/Loyalsoldierip.dat' ]; then
     echo "下载Loyalsoldierip.dat文件"
-    wget -O /usr/local/share/xray/Loyalsoldierip.dat https://raw.iqiq.io/Loyalsoldier/v2ray-rules-dat/release/geoip.dat
+    wget -O /usr/local/share/v2ray/Loyalsoldierip.dat https://raw.iqiq.io/Loyalsoldier/v2ray-rules-dat/release/geoip.dat
     else
     echo "Loyalsoldierip.dat文件存在删除下载最新版本"
-    rm -rf /usr/local/share/xray/Loyalsoldierip.dat
+    rm -rf /usr/local/share/v2ray/Loyalsoldierip.dat
     echo "下载Loyalsoldierip.dat文件"
-    wget -O /usr/local/share/xray/Loyalsoldierip.dat https://raw.iqiq.io/Loyalsoldier/v2ray-rules-dat/release/geoip.dat
+    wget -O /usr/local/share/v2ray/Loyalsoldierip.dat https://raw.iqiq.io/Loyalsoldier/v2ray-rules-dat/release/geoip.dat
 fi
 
 if [ ! -e '/usr/bin/v2raya' ]; then
