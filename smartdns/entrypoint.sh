747a73db1f17ce86574c4a8663e60c1f0493a144
zhuziba <96967229+zhuziba@users.noreply.github.com>
2023-01-21 20:32:46 +0800

    Update entrypoint.sh

--- smartdns/entrypoint.sh
+++ smartdns/entrypoint.sh
@@ -2,9 +2,9 @@
 echo -e "======================== 0.1 判断是否安装smartdns文件 ========================\n"
 
 if [ ! -e '/usr/bin/smartdns' ]; then
-    if [ $(arch) == aarch64 ]; then      wget -O /tmp/smartdns https://kgithub.com/pymumu/smartdns/releases/latest/download/smartdns-aarch64; fi
-    if [ $(arch) == x86_64 ]; then     wget -O /tmp/smartdns https://kgithub.com/pymumu/smartdns/releases/latest/download/smartdns-x86_64; fi
-    if [ $(arch) == armv7l ]; then     wget -O /tmp/smartdns https://kgithub.com/pymumu/smartdns/releases/latest/download/smartdns-arm; fi
+    if [ $(arch) == aarch64 ]; then      wget -O /tmp/smartdns https://hub.nuaa.cf/pymumu/smartdns/releases/latest/download/smartdns-aarch64; fi
+    if [ $(arch) == x86_64 ]; then     wget -O /tmp/smartdns https://hub.nuaa.cf/pymumu/smartdns/releases/latest/download/smartdns-x86_64; fi
+    if [ $(arch) == armv7l ]; then     wget -O /tmp/smartdns https://hub.nuaa.cf/pymumu/smartdns/releases/latest/download/smartdns-arm; fi
     chmod +x /tmp/smartdns
     mv /tmp/smartdns /usr/bin/smartdns
     echo "下载smartdns完成"
@@ -12,7 +12,7 @@ fi
 echo -e "======================== 0.2 判断配置文件是否存在文件 ========================\n"
 if [ ! -e '/smartdns/smartdns.conf' ]; then
     echo "smartdns.conf文件不存在开始生成配置文件"
-    wget -O /smartdns/anti-ad-smartdns.conf https://raw.kgithub.com/privacy-protection-tools/anti-AD/master/anti-ad-smartdns.conf
+    wget -O /smartdns/anti-ad-smartdns.conf https://raw.iqiq.io/privacy-protection-tools/anti-AD/master/anti-ad-smartdns.conf
     mv /tmp/smartdns.conf /smartdns/smartdns.conf
     echo "移动smartdns配置文件成功"
 fi
