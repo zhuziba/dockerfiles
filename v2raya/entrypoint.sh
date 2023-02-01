5e920f857e443b11f0433d2597246b8ca349de82
zhuziba <96967229+zhuziba@users.noreply.github.com>
2023-02-01 21:56:58 +0800

    Update entrypoint.sh

--- v2raya/entrypoint.sh
+++ v2raya/entrypoint.sh
@@ -38,7 +38,7 @@ fi
 if [ ! -e '/usr/bin/v2raya' ]; then
     if [ $(arch) == aarch64 ]; then      wget https://gh.con.sh/https://github.com/v2rayA/v2rayA/releases/download/v$VER/v2raya_linux_arm64_$VER;     chmod +x /v2raya_linux_arm64_$VER;     mv /v2raya_linux_arm64_$VER /usr/bin/v2raya; fi
     if [ $(arch) == x86_64 ]; then     wget https://gh.con.sh/https://github.com/v2rayA/v2rayA/releases/download/v$VER/v2raya_linux_x64_$VER;     chmod +x /v2raya_linux_x64_$VER;     mv /v2raya_linux_x64_$VER /usr/bin/v2raya; fi
-    if [ $(arch) == armv7l ]; then     wget https://gh.con.sh/https://github.comv2rayA/v2rayA/releases/download/v$VER/v2raya_linux_arm_$VER;     chmod +x /v2raya_linux_arm_$VER;     mv /v2raya_linux_arm_$VER /usr/bin/v2raya; fi
+    if [ $(arch) == armv7l ]; then     wget https://gh.con.sh/https://github.com/v2rayA/v2rayA/releases/download/v$VER/v2raya_linux_arm_$VER;     chmod +x /v2raya_linux_arm_$VER;     mv /v2raya_linux_arm_$VER /usr/bin/v2raya; fi
     echo "下载v2rayA完成"
 fi
 
