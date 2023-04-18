7e2a38ada486a90a6a971de414359bfccfc32777
zhuziba <96967229+zhuziba@users.noreply.github.com>
2023-04-18 16:03:36 +0800

    Update entrypoint.sh

--- v2raya/entrypoint.sh
+++ v2raya/entrypoint.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 if [ ! -e '/usr/share/v2ray/v2ray' ]; then
-    v2ray=v5.2.1
-    echo "当前获取v2ray版本为$v2ray"
-    if [ $(arch) == aarch64 ]; then      wget -O /tmp/v2ray/v2ray.zip https://gh.con.sh/https://github.com/v2fly/v2ray-core/releases/download/$v2ray/v2ray-linux-arm64-v8a.zip; fi
-    if [ $(arch) == x86_64 ]; then     wget -O /tmp/v2ray/v2ray.zip https://gh.con.sh/https://github.com/v2fly/v2ray-core/releases/download/$v2ray/v2ray-linux-64.zip; fi
-    if [ $(arch) == armv7l ]; then     wget -O /tmp/v2ray/v2ray.zip https://gh.con.sh/https://github.com/v2fly/v2ray-core/releases/download/$v2ray/v2ray-linux-arm32-v7a.zip; fi
+    if [ $(arch) == aarch64 ]; then      wget -O /tmp/v2ray/v2ray.zip https://hub.gitmirror.com/https://github.com/v2fly/v2ray-core/releases/download/v5.4.1/v2ray-linux-arm64-v8a.zip; fi
+    if [ $(arch) == x86_64 ]; then     wget -O /tmp/v2ray/v2ray.zip https://hub.gitmirror.com/https://github.com/v2fly/v2ray-core/releases/download/v5.4.1/v2ray-linux-64.zip; fi
+    if [ $(arch) == armv7l ]; then     wget -O /tmp/v2ray/v2ray.zip https://hub.gitmirror.com/https://github.com/v2fly/v2ray-core/releases/download/v5.4.1/v2ray-linux-arm32-v7a.zip; fi
     unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
     chmod +x /tmp/v2ray/v2ray
     mv /tmp/v2ray/v2ray /usr/share/v2ray/v2ray
@@ -17,28 +15,28 @@ fi
 
 if [ ! -e '/usr/local/share/v2ray/LoyalsoldierSite.dat' ]; then
     echo "下载LoyalsoldierSite.dat文件"
-    wget -O /usr/local/share/v2ray/LoyalsoldierSite.dat https://gcore.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat
+    wget -O /usr/local/share/v2ray/LoyalsoldierSite.dat https://raw.iqiq.io/Loyalsoldier/v2ray-rules-dat/release/geosite.dat
     else
     echo "LoyalsoldierSite.dat文件存在删除下载最新版本"
     rm -rf /usr/local/share/v2ray/LoyalsoldierSite.dat
     echo "下载LoyalsoldierSite.dat文件"
-    wget -O /usr/local/share/v2ray/LoyalsoldierSite.dat https://gcore.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat
+    wget -O /usr/local/share/v2ray/LoyalsoldierSite.dat https://raw.iqiq.io/Loyalsoldier/v2ray-rules-dat/release/geosite.dat
 fi
 
 if [ ! -e '/usr/local/share/v2ray/Loyalsoldierip.dat' ]; then
     echo "下载Loyalsoldierip.dat文件"
-    wget -O /usr/local/share/v2ray/Loyalsoldierip.dat https://gcore.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat
+    wget -O /usr/local/share/v2ray/Loyalsoldierip.dat https://raw.iqiq.io/Loyalsoldier/v2ray-rules-dat/release/geoip.dat
     else
     echo "Loyalsoldierip.dat文件存在删除下载最新版本"
     rm -rf /usr/local/share/v2ray/Loyalsoldierip.dat
     echo "下载Loyalsoldierip.dat文件"
-    wget -O /usr/local/share/v2ray/Loyalsoldierip.dat https://gcore.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat
+    wget -O /usr/local/share/v2ray/Loyalsoldierip.dat https://raw.iqiq.io/Loyalsoldier/v2ray-rules-dat/release/geoip.dat
 fi
 
 if [ ! -e '/usr/bin/v2raya' ]; then
-    if [ $(arch) == aarch64 ]; then      wget https://gh.con.sh/https://github.com/v2rayA/v2rayA/releases/download/v$VER/v2raya_linux_arm64_$VER;     chmod +x /v2raya_linux_arm64_$VER;     mv /v2raya_linux_arm64_$VER /usr/bin/v2raya; fi
-    if [ $(arch) == x86_64 ]; then     wget https://gh.con.sh/https://github.com/v2rayA/v2rayA/releases/download/v$VER/v2raya_linux_x64_$VER;     chmod +x /v2raya_linux_x64_$VER;     mv /v2raya_linux_x64_$VER /usr/bin/v2raya; fi
-    if [ $(arch) == armv7l ]; then     wget https://gh.con.sh/https://github.com/v2rayA/v2rayA/releases/download/v$VER/v2raya_linux_arm_$VER;     chmod +x /v2raya_linux_arm_$VER;     mv /v2raya_linux_arm_$VER /usr/bin/v2raya; fi
+    if [ $(arch) == aarch64 ]; then      wget https://hub.gitmirror.com/https://github.com/v2rayA/v2rayA/releases/download/v$VER/v2raya_linux_arm64_$VER;     chmod +x /v2raya_linux_arm64_$VER;     mv /v2raya_linux_arm64_$VER /usr/bin/v2raya; fi
+    if [ $(arch) == x86_64 ]; then     wget https://hub.gitmirror.com/https://github.com/v2rayA/v2rayA/releases/download/v$VER/v2raya_linux_x64_$VER;     chmod +x /v2raya_linux_x64_$VER;     mv /v2raya_linux_x64_$VER /usr/bin/v2raya; fi
+    if [ $(arch) == armv7l ]; then     wget https://hub.gitmirror.com/https://github.com/v2rayA/v2rayA/releases/download/v$VER/v2raya_linux_arm_$VER;     chmod +x /v2raya_linux_arm_$VER;     mv /v2raya_linux_arm_$VER /usr/bin/v2raya; fi
     echo "下载v2rayA完成"
 fi
 
@@ -46,9 +44,9 @@ if [ ! -e '/etc/v2raya/smartdns.conf' ]; then
     echo "smartdns.conf文件不存在不启动smartdns"
     else
     echo "smartdns.conf文件存在启动smartdns"
-    if [ $(arch) == aarch64 ]; then      wget -O /tmp/smartdns https://gh.con.sh/https://github.com/pymumu/smartdns/releases/latest/download/smartdns-aarch64; fi
-    if [ $(arch) == x86_64 ]; then     wget -O /tmp/smartdns https://gh.con.sh/https://github.com/pymumu/smartdns/releases/latest/download/smartdns-x86_64; fi
-    if [ $(arch) == armv7l ]; then     wget -O /tmp/smartdns https://gh.con.sh/https://github.com/pymumu/smartdns/releases/latest/download/smartdns-arm; fi
+    if [ $(arch) == aarch64 ]; then      wget -O /tmp/smartdns https://hub.gitmirror.com/https://github.com/pymumu/smartdns/releases/latest/download/smartdns-aarch64; fi
+    if [ $(arch) == x86_64 ]; then     wget -O /tmp/smartdns https://hub.gitmirror.com/https://github.com/pymumu/smartdns/releases/latest/download/smartdns-x86_64; fi
+    if [ $(arch) == armv7l ]; then     wget -O /tmp/smartdns https://hub.gitmirror.com/https://github.com/pymumu/smartdns/releases/latest/download/smartdns-arm; fi
     chmod +x /tmp/smartdns
     mv /tmp/smartdns /usr/bin/smartdns
     echo "下载smartdns完成"
@@ -57,6 +55,4 @@ fi
 
 echo "启动v2raya"
 v2raya
-#pm2 start v2raya --name v2raya
 #v2raya --log-level error --log-file /var/log/v2raya.log
-tail -f /dev/null
