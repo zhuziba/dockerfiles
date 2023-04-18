332a768f9da39c0e164a6e058a8342961c901ab9
zhuziba <96967229+zhuziba@users.noreply.github.com>
2023-04-18 19:09:26 +0800

    .

--- blocky/entrypoint.sh
+++ blocky/entrypoint.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
-blocky --config /blocky/config.yaml
-tail -f /dev/null
+if [ ! -e '/blocky/redis.conf' ]; then
+    echo "redis.conf文件不存在不启动redis"
+    else
+    echo "redis.conf文件存在启动redis"
+    sysctl vm.overcommit_memory=1
+    redis-server /blocky/redis.conf
+fi
+
+cat <<EOF> /etc/supervisord.conf
+[supervisord]
+loglevel=info 
+nodaemon=true
+user=root
+[program:blocky]
+user=root
+command=/usr/bin/blocky --config /blocky/config.yaml
+EOF
+if [[ $down_type == git ]]; then
+    echo "变量配置了远程配置运远程配置"
+    wget ${down_url} -O /blocky/config.yaml
+    else
+    echo "变量未配置远程文件运行本地配置"
+fi
+echo "由于dns比较重要采用supervisord启动守护"
+supervisord -c /etc/supervisord.conf
