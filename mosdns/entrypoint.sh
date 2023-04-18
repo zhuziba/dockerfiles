f43c92483c8446d29c3ac10bf2d7622baee2f62f
zhuziba <96967229+zhuziba@users.noreply.github.com>
2023-04-18 16:26:05 +0800

    .

--- mosdns/entrypoint.sh
+++ mosdns/entrypoint.sh
@@ -1,26 +1,52 @@
 #!/bin/bash
-echo "开始生成nginx配置文件"
-cat <<EOF> /etc/nginx/http.d/default.conf
-server {
-	listen 80 default_server;
-	#listen [::]:80 default_server;
-	charset utf-8;
+if [ ! -e '/mosdns/cache.dump' ]; then
+    echo "cache.dump文件不存在创建cache.dump"
+    touch /mosdns/cache.dump
+fi
 
-     location /dns-query {
-     set_real_ip_from 0.0.0.0/0;
-     real_ip_header X-Forwarded-For;
-     proxy_pass http://127.0.0.1:8888/dns-query;
-     }
-}
-EOF
-echo "启动nginx进程"
-nginx
-echo "每天2点自动下载geoip和geosite文件"
-echo '0 2 * * *  wget -O /mosdns/geoip.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat'>>/var/spool/cron/crontabs/root
-echo '0 2 * * *  wget -O /mosdns/geosite.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat'>>/var/spool/cron/crontabs/root
+if [ ! -e '/mosdns/redis.conf' ]; then
+    echo "redis.conf文件不存在不启动redis"
+    else
+    echo "redis.conf文件存在启动redis"
+    sysctl vm.overcommit_memory=1
+    redis-server /mosdns/redis.conf
+fi
+
+echo -e "======================== 1.0 是否启动diy脚本========================\n"
+if [ ! -e '/mosdns/diy.sh' ]; then
+    echo "目录不存在diy.sh文件不执行diy脚本"
+    else
+    echo "目录存在diy.sh文件执行diy脚本"
+    bash /mosdns/diy.sh
+fi
 echo "启动定时任务"
 crond -b -l 8
-echo "启动openrc后台启动"
-openrc boot
-/etc/init.d/mosdns start
-tail -f /dev/null
+cat <<EOF> /etc/supervisord.conf
+[unix_http_server]
+file=/tmp/supervisor.sock
+[supervisord]
+loglevel=info 
+nodaemon=true
+user=root
+[program:mosdns]
+user=root
+command=/usr/bin/mosdns start -c /mosdns/config.yaml
+[rpcinterface:supervisor]
+supervisor.rpcinterface_factory = supervisor.rpcinterface:make_main_rpcinterface
+[supervisorctl]
+serverurl=unix:///tmp/supervisor.sock
+EOF
+if [[ $down_type == git ]]; then
+    echo "变量配置了远程配置运远程配置"
+    wget ${down_url} -O /mosdns/config.yaml
+    else
+    echo "变量未配置远程文件运行本地配置"
+fi
+if [ ! -e '/mosdns/supervisord.conf' ]; then
+    echo "目录不存在supervisord.conf不替换"
+    else
+    echo "目录存在supervisord.conf开始替换supervisord"
+    cp /mosdns/supervisord.conf /etc/supervisord.conf
+fi
+supervisord -c /etc/supervisord.conf
+#tail -f /dev/null
