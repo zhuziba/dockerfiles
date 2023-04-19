8f56e121cadbeb642f280c2c451379cb453795cd
zhuziba <96967229+zhuziba@users.noreply.github.com>
2023-04-19 20:01:03 +0800

    .

--- v2raya/entrypoint.sh
+++ v2raya/entrypoint.sh
@@ -22,21 +22,7 @@ fi
 if [ ! -e '/usr/bin/v2raya' ]; then
     if [ $(arch) == aarch64 ]; then      wget https://hub.gitmirror.com/https://github.com/v2rayA/v2rayA/releases/download/v$VER/v2raya_linux_arm64_$VER;     chmod +x /v2raya_linux_arm64_$VER;     mv /v2raya_linux_arm64_$VER /usr/bin/v2raya; fi
     if [ $(arch) == x86_64 ]; then     wget https://hub.gitmirror.com/https://github.com/v2rayA/v2rayA/releases/download/v$VER/v2raya_linux_x64_$VER;     chmod +x /v2raya_linux_x64_$VER;     mv /v2raya_linux_x64_$VER /usr/bin/v2raya; fi
-    if [ $(arch) == armv7l ]; then     wget https://hub.gitmirror.com/https://github.com/v2rayA/v2rayA/releases/download/v$VER/v2raya_linux_arm_$VER;     chmod +x /v2raya_linux_arm_$VER;     mv /v2raya_linux_arm_$VER /usr/bin/v2raya; fi
     echo "下载v2rayA完成"
-fi
-
-if [ ! -e '/etc/v2raya/smartdns.conf' ]; then
-    echo "smartdns.conf文件不存在不启动smartdns"
-    else
-    echo "smartdns.conf文件存在启动smartdns"
-    if [ $(arch) == aarch64 ]; then      wget -O /tmp/smartdns https://hub.gitmirror.com/https://github.com/pymumu/smartdns/releases/latest/download/smartdns-aarch64; fi
-    if [ $(arch) == x86_64 ]; then     wget -O /tmp/smartdns https://hub.gitmirror.com/https://github.com/pymumu/smartdns/releases/latest/download/smartdns-x86_64; fi
-    if [ $(arch) == armv7l ]; then     wget -O /tmp/smartdns https://hub.gitmirror.com/https://github.com/pymumu/smartdns/releases/latest/download/smartdns-arm; fi
-    chmod +x /tmp/smartdns
-    mv /tmp/smartdns /usr/bin/smartdns
-    echo "下载smartdns完成"
-    smartdns -c /etc/v2raya/smartdns.conf
 fi
 
 echo "启动v2raya"
