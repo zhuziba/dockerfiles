e93f131f6f4c187fa1eed9135e6107df68ed8ff4
zhuziba <96967229+zhuziba@users.noreply.github.com>
2023-04-19 15:18:36 +0800

    .

--- v2raya/entrypoint.sh
+++ v2raya/entrypoint.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
-
-if [ ! -e '/usr/share/v2ray/v2ray' ]; then
-    if [ $(arch) == aarch64 ]; then      wget -O /tmp/v2ray/v2ray.zip https://hub.gitmirror.com/https://github.com/v2fly/v2ray-core/releases/download/v5.4.1/v2ray-linux-arm64-v8a.zip; fi
-    if [ $(arch) == x86_64 ]; then     wget -O /tmp/v2ray/v2ray.zip https://hub.gitmirror.com/https://github.com/v2fly/v2ray-core/releases/download/v5.4.1/v2ray-linux-64.zip; fi
-    if [ $(arch) == armv7l ]; then     wget -O /tmp/v2ray/v2ray.zip https://hub.gitmirror.com/https://github.com/v2fly/v2ray-core/releases/download/v5.4.1/v2ray-linux-arm32-v7a.zip; fi
-    unzip /tmp/v2ray/v2ray.zip -d /tmp/v2ray
-    chmod +x /tmp/v2ray/v2ray
-    mv /tmp/v2ray/v2ray /usr/share/v2ray/v2ray
-    mv /tmp/v2ray/geoip.dat /usr/local/share/v2ray/geoip.dat
-    mv /tmp/v2ray/geosite.dat /usr/local/share/v2ray/geosite.dat
-    rm -rf /tmp/v2ray
-    echo "下载v2ray完成"
-fi
-
 if [ ! -e '/usr/local/share/v2ray/LoyalsoldierSite.dat' ]; then
     echo "下载LoyalsoldierSite.dat文件"
     wget -O /usr/local/share/v2ray/LoyalsoldierSite.dat https://raw.iqiq.io/Loyalsoldier/v2ray-rules-dat/release/geosite.dat
