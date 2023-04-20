38e968957bc4b919860c607c22a1cbdb6b4af6e8
zhuziba <96967229+zhuziba@users.noreply.github.com>
2023-04-20 13:19:48 +0800

    .

--- smartdns/entrypoint.sh
+++ smartdns/entrypoint.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
-echo -e "======================== 0.1 判断是否安装smartdns文件 ========================\n"
-
-if [ ! -e '/usr/bin/smartdns' ]; then
-    if [ $(arch) == aarch64 ]; then      wget -O /tmp/smartdns https://hub.nuaa.cf/pymumu/smartdns/releases/latest/download/smartdns-aarch64; fi
-    if [ $(arch) == x86_64 ]; then     wget -O /tmp/smartdns https://hub.nuaa.cf/pymumu/smartdns/releases/latest/download/smartdns-x86_64; fi
-    if [ $(arch) == armv7l ]; then     wget -O /tmp/smartdns https://hub.nuaa.cf/pymumu/smartdns/releases/latest/download/smartdns-arm; fi
-    chmod +x /tmp/smartdns
-    mv /tmp/smartdns /usr/bin/smartdns
-    echo "下载smartdns完成"
-fi
-echo -e "======================== 0.2 判断配置文件是否存在文件 ========================\n"
-if [ ! -e '/smartdns/smartdns.conf' ]; then
-    echo "smartdns.conf文件不存在开始生成配置文件"
-    wget -O /smartdns/anti-ad-smartdns.conf https://raw.iqiq.io/privacy-protection-tools/anti-AD/master/anti-ad-smartdns.conf
-    mv /tmp/smartdns.conf /smartdns/smartdns.conf
-    echo "移动smartdns配置文件成功"
-fi
-smartdns -c /smartdns/smartdns.conf
-echo "smartdns启动成功"
-echo -e "======================== 0.3. 是否启动diy脚本========================\n"
+echo -e "======================== 1.0 是否启动diy脚本========================\n"
 if [ ! -e '/smartdns/diy.sh' ]; then
     echo "目录不存在diy.sh文件不执行diy脚本"
     else
     echo "目录存在diy.sh文件执行diy脚本"
     bash /smartdns/diy.sh
 fi
+echo "开始生成openrc启动配置文件"
+cat <<EOF> /etc/init.d/smartdns
+#!/sbin/openrc-run
+name="smartdns"
+command="/usr/bin/smartdns"
+command_args="-c /smartdns/smartdns.conf"
+command_background=false
+pidfile="/run/${RC_SVCNAME}.pid"
+EOF
+chmod +x /etc/init.d/smartdns
+rc-update add smartdns
+openrc default
+#rc-service smartdns start
+#smartdns -c /smartdns/smartdns.conf
+echo "smartdns启动成功"
 tail -f /dev/null
