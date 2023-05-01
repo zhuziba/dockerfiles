d275ffe09be0dbc052d7d0806afee8e6bf7156f7
zhuziba <96967229+zhuziba@users.noreply.github.com>
2023-05-01 09:59:36 +0800

    .

--- v2raya/entrypoint.sh
+++ v2raya/entrypoint.sh
@@ -19,12 +19,6 @@ if [ ! -e '/usr/local/share/v2ray/Loyalsoldierip.dat' ]; then
     wget -O /usr/local/share/v2ray/Loyalsoldierip.dat https://raw.iqiq.io/Loyalsoldier/v2ray-rules-dat/release/geoip.dat
 fi
 
-if [ ! -e '/usr/bin/v2raya' ]; then
-    if [ $(arch) == aarch64 ]; then      wget https://hub.gitmirror.com/https://github.com/v2rayA/v2rayA/releases/download/v$VER/v2raya_linux_arm64_$VER;     chmod +x /v2raya_linux_arm64_$VER;     mv /v2raya_linux_arm64_$VER /usr/bin/v2raya; fi
-    if [ $(arch) == x86_64 ]; then     wget https://hub.gitmirror.com/https://github.com/v2rayA/v2rayA/releases/download/v$VER/v2raya_linux_x64_$VER;     chmod +x /v2raya_linux_x64_$VER;     mv /v2raya_linux_x64_$VER /usr/bin/v2raya; fi
-    echo "下载v2rayA完成"
-fi
-
 echo "启动v2raya"
 v2raya
 #v2raya --log-level error --log-file /var/log/v2raya.log
