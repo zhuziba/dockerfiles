db3b7b0b59ac06614dcd8eb7e534d3206d031d08
zhuziba <96967229+zhuziba@users.noreply.github.com>
2023-07-26 23:06:14 +0800

    .

--- technitium/entrypoint.sh
+++ technitium/entrypoint.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 
-/usr/bin/dotnet /DnsServer/DnsServerApp.dll
+
+cat <<EOF> /etc/supervisord.conf
+[unix_http_server]
+file=/tmp/supervisor.sock
+[supervisord]
+loglevel=info 
+nodaemon=true
+user=root
+[program:mosdns]
+user=root
+command=/usr/bin/dotnet /DnsServer/DnsServerApp.dll
+[rpcinterface:supervisor]
+supervisor.rpcinterface_factory = supervisor.rpcinterface:make_main_rpcinterface
+[supervisorctl]
+serverurl=unix:///tmp/supervisor.sock
+EOF
+
+if [ ! -e '/DnsServer/config/supervisord.conf' ]; then
+    echo "目录不存在supervisord.conf不替换"
+    else
+    echo "目录存在supervisord.conf开始替换supervisord"
+    cp /DnsServer/config/supervisord.conf /etc/supervisord.conf
+fi
+supervisord -c /etc/supervisord.conf
 tail -f /dev/null
