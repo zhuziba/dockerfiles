7156c7072bb9ef01c221870aaccd850944126566
zhuziba <96967229+zhuziba@users.noreply.github.com>
2023-09-17 18:11:24 +0800

    Update entrypoint.sh

--- cdns/entrypoint.sh
+++ cdns/entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+if [ ! -e '/cdns/redis.conf' ]; then
+    echo "redis.conf文件不存在不启动redis"
+    else
+    echo "redis.conf文件存在启动redis"
+    sysctl vm.overcommit_memory=1
+    redis-server /cdns/redis.conf
+fi
 /usr/bin/cdns run -c /cdns/config.yaml
 tail -f /dev/null
