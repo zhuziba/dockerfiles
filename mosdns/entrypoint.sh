4bc01f1309a641e39ee8b303685ca24313fb1896
zhuziba <96967229+zhuziba@users.noreply.github.com>
2023-12-17 17:16:02 +0800

    Update entrypoint.sh

--- mosdns/entrypoint.sh
+++ mosdns/entrypoint.sh
@@ -6,21 +6,6 @@ if [ ! -e '/mosdns/diy.sh' ]; then
     echo "目录存在diy.sh文件执行diy脚本"
     bash /mosdns/diy.sh
 fi
-cat <<EOF> /etc/supervisord.conf
-[unix_http_server]
-file=/tmp/supervisor.sock
-[supervisord]
-loglevel=info 
-nodaemon=true
-user=root
-[program:mosdns]
-user=root
-command=/usr/bin/mosdns start -c /mosdns/config.yaml
-[rpcinterface:supervisor]
-supervisor.rpcinterface_factory = supervisor.rpcinterface:make_main_rpcinterface
-[supervisorctl]
-serverurl=unix:///tmp/supervisor.sock
-EOF
 if [[ $down_type == git ]]; then
     echo "变量配置了远程配置运远程配置"
     wget ${down_url} -O /mosdns/config.yaml
@@ -29,8 +14,9 @@ if [[ $down_type == git ]]; then
 fi
 if [ ! -e '/mosdns/supervisord.conf' ]; then
     echo "目录不存在supervisord.conf不替换"
+    /usr/bin/mosdns start -c /mosdns/config.yaml
     else
     echo "目录存在supervisord.conf开始替换supervisord"
     cp /mosdns/supervisord.conf /etc/supervisord.conf
+    supervisord -c /etc/supervisord.conf
 fi
-supervisord -c /etc/supervisord.conf
