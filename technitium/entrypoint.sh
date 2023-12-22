ed17b412c41149e1c4eff16f64e8b06f82769b82
zhuziba <96967229+zhuziba@users.noreply.github.com>
2023-12-23 02:45:37 +0800

    Update entrypoint.sh

--- technitium/entrypoint.sh
+++ technitium/entrypoint.sh
@@ -1,26 +1,3 @@
 #!/bin/bash
-
-
-cat <<EOF> /etc/supervisord.conf
-[unix_http_server]
-file=/tmp/supervisor.sock
-[supervisord]
-loglevel=info 
-nodaemon=true
-user=root
-[program:dotnet]
-user=root
-command=/usr/bin/dotnet /DnsServer/DnsServerApp.dll
-[rpcinterface:supervisor]
-supervisor.rpcinterface_factory = supervisor.rpcinterface:make_main_rpcinterface
-[supervisorctl]
-serverurl=unix:///tmp/supervisor.sock
-EOF
-
-if [ ! -e '/DnsServer/config/supervisord.conf' ]; then
-    echo "目录不存在supervisord.conf不替换"
-    else
-    echo "目录存在supervisord.conf开始替换supervisord"
-    cp /DnsServer/config/supervisord.conf /etc/supervisord.conf
-fi
-supervisord -c /etc/supervisord.conf
+/usr/bin/dotnet /DnsServer/DnsServerApp.dll
+tail -f /dev/null
