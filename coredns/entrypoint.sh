578daaa4666e07a00070944337b4de61f6e70e91
zhuziba <96967229+zhuziba@users.noreply.github.com>
2024-01-04 02:24:23 +0800

    Update entrypoint.sh

--- coredns/entrypoint.sh
+++ coredns/entrypoint.sh
@@ -3,5 +3,13 @@ if [ ! -e '/coredns/Corefile' ]; then
     echo "Corefile配置文件不存在请配置好在启动docker"
     touch /coredns/Corefile
 fi
+
+if [ ! -e '/coredns/redis.conf' ]; then
+    echo "redis.conf文件不存在不启动redis"
+    else
+    echo "redis.conf文件存在启动redis"
+    sysctl vm.overcommit_memory=1
+    redis-server /coredns/redis.conf
+fi
 /usr/bin/coredns -conf /coredns/Corefile
 tail -f /dev/null
