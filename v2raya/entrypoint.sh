3bd93b2466020fd196af5aba92906a7c7d760ed8
zhuziba <96967229+zhuziba@users.noreply.github.com>
2024-01-09 11:36:40 +0800

    .

--- v2raya/entrypoint.sh
+++ v2raya/entrypoint.sh
@@ -21,4 +21,12 @@ fi
 
 echo "启动v2raya"
 #v2raya
-v2raya --log-level error --log-file /var/log/v2raya.log
+if [ ! -e '/etc/v2raya/supervisord.conf' ]; then
+    echo "目录不存在supervisord.conf不替换"
+    /usr/bin/v2raya --log-level error --log-file /var/log/v2raya.log
+    else
+    echo "目录存在supervisord.conf开始替换supervisord"
+    cp /etc/v2raya/supervisord.conf /etc/supervisord.conf
+    supervisord -c /etc/supervisord.conf
+fi
+tail -f /dev/null
