d82cad9874e4d4c6f8f8392f59ae38d994fcf30b
zhuziba <96967229+zhuziba@users.noreply.github.com>
2024-02-12 13:54:01 +0800

    Update entrypoint.sh

--- v2raya/entrypoint.sh
+++ v2raya/entrypoint.sh
@@ -1,24 +1,4 @@
 #!/bin/bash
-if [ ! -e '/usr/local/share/v2ray/LoyalsoldierSite.dat' ]; then
-    echo "下载LoyalsoldierSite.dat文件"
-    wget -O /usr/local/share/v2ray/LoyalsoldierSite.dat https://raw.fgit.cf/Loyalsoldier/v2ray-rules-dat/release/geosite.dat
-    else
-    echo "LoyalsoldierSite.dat文件存在删除下载最新版本"
-    rm -rf /usr/local/share/v2ray/LoyalsoldierSite.dat
-    echo "下载LoyalsoldierSite.dat文件"
-    wget -O /usr/local/share/v2ray/LoyalsoldierSite.dat https://raw.fgit.cf/Loyalsoldier/v2ray-rules-dat/release/geosite.dat
-fi
-
-if [ ! -e '/usr/local/share/v2ray/Loyalsoldierip.dat' ]; then
-    echo "下载Loyalsoldierip.dat文件"
-    wget -O /usr/local/share/v2ray/Loyalsoldierip.dat https://raw.fgit.cf/Loyalsoldier/v2ray-rules-dat/release/geoip.dat
-    else
-    echo "Loyalsoldierip.dat文件存在删除下载最新版本"
-    rm -rf /usr/local/share/v2ray/Loyalsoldierip.dat
-    echo "下载Loyalsoldierip.dat文件"
-    wget -O /usr/local/share/v2ray/Loyalsoldierip.dat https://raw.fgit.cf/Loyalsoldier/v2ray-rules-dat/release/geoip.dat
-fi
-
 echo "启动v2raya"
 #v2raya
 if [ ! -e '/etc/v2raya/supervisord.conf' ]; then
