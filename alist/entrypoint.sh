374e074ff5e85348d88ef4088735e87b985a3e1b
zhuziba <96967229+zhuziba@users.noreply.github.com>
2024-06-28 16:11:16 +0800

    Update entrypoint.sh

--- alist/entrypoint.sh
+++ alist/entrypoint.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-
-chown -R ${PUID}:${PGID} /opt/alist/
-
-umask ${UMASK}
-
 if [ "$1" = "version" ]; then
   /usr/bin/alist version
 else
-  exec su-exec ${PUID}:${PGID} /usr/bin/alist server --no-prefix
+  /usr/bin/alist server --no-prefix
 fi
