591b2870a23c499ba3451a4c939e848b8dc8d5d3
zhuziba <96967229+zhuziba@users.noreply.github.com>
2024-10-06 00:21:47 +0800

    Update entrypoint.sh

--- cdns/entrypoint.sh
+++ cdns/entrypoint.sh
@@ -6,5 +6,12 @@ if [ ! -e '/cdns/redis.conf' ]; then
     sysctl vm.overcommit_memory=1
     redis-server /cdns/redis.conf
 fi
-/usr/bin/mosproxy router -c /cdns/mosproxy.yaml
+if [ ! -e '/cdns/supervisord.conf' ]; then
+    echo "目录不存在supervisord.conf不替换"
+    /usr/bin/mosproxy router -c /cdns/mosproxy.yaml
+    else
+    echo "目录存在supervisord.conf开始替换supervisord"
+    cp /cdns/supervisord.conf /etc/supervisord.conf
+    supervisord -c /etc/supervisord.conf
+fi
 tail -f /dev/null
