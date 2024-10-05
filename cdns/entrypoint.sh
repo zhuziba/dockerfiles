6a56dc1465071fd0d308ef21c0f91c8b12587a03
zhuziba <96967229+zhuziba@users.noreply.github.com>
2024-10-06 00:56:28 +0800

    Update entrypoint.sh

--- cdns/entrypoint.sh
+++ cdns/entrypoint.sh
@@ -6,6 +6,14 @@ if [ ! -e '/cdns/redis.conf' ]; then
     sysctl vm.overcommit_memory=1
     redis-server /cdns/redis.conf
 fi
+echo -e "======================== 1.0 是否启动diy脚本========================\n"
+if [ ! -e '/cdns/diy.sh' ]; then
+    echo "目录不存在diy.sh文件不执行diy脚本"
+    else
+    echo "目录存在diy.sh文件执行diy脚本"
+    bash /cdns/diy.sh
+fi
+echo -e "======================== 1.1 是否启动supervisord========================\n"
 if [ ! -e '/cdns/supervisord.conf' ]; then
     echo "目录不存在supervisord.conf不替换"
     /usr/bin/mosproxy router -c /cdns/mosproxy.yaml
