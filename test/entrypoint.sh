5866ca37e9168963b8d033f989c6402b63fce2a5
zhuziba <96967229+zhuziba@users.noreply.github.com>
2025-01-21 21:16:22 +0800

    Update entrypoint.sh

--- test/entrypoint.sh
+++ test/entrypoint.sh
@@ -6,11 +6,4 @@ if [ ! -e '/test/diy.sh' ]; then
     echo "目录存在diy.sh文件执行diy脚本"
     bash /test/diy.sh
 fi
-
-if [ ! -e '/test/supervisord.conf' ]; then
-    echo "目录不存在supervisord.conf不替换"
-    else
-    echo "目录存在supervisord.conf开始替换supervisord"
-    cp /test/supervisord.conf /etc/supervisord.conf
-fi
 tail -f /dev/null
