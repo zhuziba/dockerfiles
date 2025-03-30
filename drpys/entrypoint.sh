d53ac0a4a8e798ec6cf7ddd0f42f7b384e6d1e1e
zhuziba <96967229+zhuziba@users.noreply.github.com>
2025-03-30 22:35:48 +0800

    add

--- drpys/entrypoint.sh
+++ drpys/entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+echo -e "======================== 0.1. 是否启动diy脚本========================\n"
+if [ ! -e '/drpys/diy.sh' ]; then
+    echo "目录不存在diy.sh文件不执行diy脚本"
+    else
+    echo "目录存在diy.sh文件执行diy脚本"
+    bash /drpys/diy.sh
+fi
 cd /usr/local/app && pm2 start index.js --name drpys
 tail -f /dev/null
