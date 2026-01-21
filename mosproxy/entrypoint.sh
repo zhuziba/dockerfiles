4edd4518afb7fab2b876a306d617a906c6b5e1ee
GitHub Action <action@github.com>
2026-01-21 15:55:14 +0800

    Add changes

--- mosproxy/entrypoint.sh
+++ mosproxy/entrypoint.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-if [ ! -e '/mosproxy/redis.conf' ]; then
-    echo "redis.conf文件不存在不启动redis"
-    else
-    echo "redis.conf文件存在启动redis"
-    sysctl vm.overcommit_memory=1
-    redis-server /mosproxy/redis.conf
-fi
 echo -e "======================== 1.0 是否启动diy脚本========================\n"
 if [ ! -e '/mosproxy/diy.sh' ]; then
     echo "目录不存在diy.sh文件不执行diy脚本"
