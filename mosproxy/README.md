304893e9a281c2ad58bad8150d8d3e380f108e24
GitHub Action <action@github.com>
2026-01-21 15:53:36 +0800

    Add changes

--- mosproxy/README.md
+++ mosproxy/README.md
@@ -1,4 +1,4 @@
-# cdns
+# mosproxy
 
 开启混杂模式
 
@@ -9,4 +9,4 @@ docker创建网络,注意将网段改为你自己的
 
 ``docker network create -d macvlan --subnet=192.168.50.0/24 --gateway=192.168.50.1 -o parent=eth0 macnet``
 
-``docker run -dit --restart=always --name cdns -d -v /docker/cdns:/cdns --network macnet --ip 192.168.50.254 --privileged byxiaopeng/cdns:latest``
+``docker run -dit --restart=always --name mosproxy -d -v /docker/mosproxy:/mosproxy --network macnet --ip 192.168.50.254 --privileged byxiaopeng/mosproxy:latest``
--- mosproxy/entrypoint.sh
+++ mosproxy/entrypoint.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
-if [ ! -e '/cdns/redis.conf' ]; then
+if [ ! -e '/mosproxy/redis.conf' ]; then
     echo "redis.conf文件不存在不启动redis"
     else
     echo "redis.conf文件存在启动redis"
     sysctl vm.overcommit_memory=1
-    redis-server /cdns/redis.conf
+    redis-server /mosproxy/redis.conf
 fi
 echo -e "======================== 1.0 是否启动diy脚本========================\n"
-if [ ! -e '/cdns/diy.sh' ]; then
+if [ ! -e '/mosproxy/diy.sh' ]; then
     echo "目录不存在diy.sh文件不执行diy脚本"
     else
     echo "目录存在diy.sh文件执行diy脚本"
-    bash /cdns/diy.sh
+    bash /mosproxy/diy.sh
 fi
 echo -e "======================== 1.1 是否启动supervisord========================\n"
-if [ ! -e '/cdns/supervisord.conf' ]; then
+if [ ! -e '/mosproxy/supervisord.conf' ]; then
     echo "目录不存在supervisord.conf不替换"
-    /usr/bin/mosproxy router -c /cdns/mosproxy.yaml
+    /usr/bin/mosproxy router -c /mosproxy/mosproxy.yaml
     else
     echo "目录存在supervisord.conf开始替换supervisord"
-    cp /cdns/supervisord.conf /etc/supervisord.conf
+    cp /mosproxy/supervisord.conf /etc/supervisord.conf
     supervisord -c /etc/supervisord.conf
 fi
 tail -f /dev/null
