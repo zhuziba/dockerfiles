c92021c8c0c5289448301d3c38fe7af1d1bd54fa
GitHub Action <action@github.com>
2026-03-26 22:25:43 +0800

    Add changes

---
+++ drpyhouse/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo -e "======================== 0.1. 是否启动diy脚本========================\n"
+if [ ! -e '/drpys/diy.sh' ]; then
+    echo "目录不存在diy.sh文件不执行diy脚本"
+    else
+    echo "目录存在diy.sh文件执行diy脚本"
+    bash /drpys/diy.sh
+fi
+cd /usr/local/app && bun run pm2:start
+tail -f /dev/null
